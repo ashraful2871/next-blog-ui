b6a8aa0b76afe0085202c342bdf9c83fd2d71d26
ashraful2871 <ashrafulislam2758@gmail.com>
2025-09-21 19:13:59 +0600

    first commit

---
+++ src/app/(public)/blogs/loading.tsx
@@ -0,0 +1,12 @@
+import Loading from "@/components/ui/Loading";
+import React from "react";
+
+const BlogLoadingPAge = () => {
+  return (
+    <div className="py-40">
+      <Loading></Loading>
+    </div>
+  );
+};
+
+export default BlogLoadingPAge;
---
+++ src/app/(public)/blogs/page.tsx
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import BlogCard from "@/components/modules/Blogs/BlogCard";
+
+const AllBlogsPage = async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/post`, {
+    cache: "no-store",
+  });
+  const { data: blogs } = await res.json();
+  return (
+    <div className="py-30 px-4 max-w-7xl mx-auto">
+      <h2 className="text-center text-4xl">All Blogs</h2>
+
+      <div className="grid grid-cols-3 gap-5 max-w-6xl mx-auto my-5">
+        {blogs.map((blogs: any) => (
+          <BlogCard key={blogs.id} post={blogs}></BlogCard>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default AllBlogsPage;
